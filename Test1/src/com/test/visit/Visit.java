dbad813838d7a5bdaccf5da77bba3a4abbbdbfa8
jack <415876780@qq.com>
2016-08-17 17:46:29 +0800

    现在有一个城市销售经理，需要从公司出发，去拜访市内的商家，已知他的位置以及商家的位置，但是由于城市道路交通的原因，他只能在左右中选择一个方向，在上下中选择一个方向，现在问他有多少种方案到达商家地址。

---
+++ Test1/src/com/test/visit/Visit.java
@@ -0,0 +1,69 @@
+package com.test.visit;
+
+public class Visit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] map=new int[][]{{0,1,0,},{0,0,2}};
+		int n=2;
+		int m=3;
+		System.out.println(new Visit().countPath(map, n, m));
+		
+	}
+	public int countPath(int[][] map, int n, int m) {
+        // write code here
+		int x1=0,x2=0,y1=0,y2=0;
+		for(int i=0;i<n;i++){
+			for(int j=0;j<m;j++){
+				if(map[i][j]==1){
+					x1=i;
+					y1=j;
+				}
+				if(map[i][j]==2){
+					x2=i;
+					y2=j;
+				}
+			}
+		}
+		
+		return visit(map,x2,y2,x1,y1,x2,y2);
+	}
+	public int visit(int[][] map,int x,int y,int m,int n,int m1,int n1){
+
+		
+		
+		if(m1>=m&&n1>=n){
+			if(x<m||y<n) return 0;
+			if(map[x][y]==1){
+				return 1;
+			}
+			if(map[x][y]==-1)return 0;
+			return visit(map,x-1,y,m,n,m1,n1)+visit(map,x,y-1,m,n,m1,n1);
+		}
+		 if(m1>=m&&n1<n){
+			 if(x<m||y>n) return 0;
+			 if(map[x][y]==1){
+					return 1;
+				}
+			 if(map[x][y]==-1)return 0;
+			return visit(map,x-1,y,m,n,m1,n1)+visit(map,x,y+1,m,n,m1,n1);
+			}
+		 if(m1<m&&n1>=n){
+			 if(x>m||y<n) return 0;
+			 if(map[x][y]==1){
+					return 1;
+				}
+			 if(map[x][y]==-1)return 0;
+			return visit(map,x+1,y,m,n,m1,n1)+visit(map,x,y-1,m,n,m1,n1);
+			}
+		 if(m1<m&&n1<n){
+			 if(x>m||y>n) return 0;
+			 if(map[x][y]==1){
+					return 1;
+				}
+			 if(map[x][y]==-1)return 0;
+			return visit(map,x+1,y,m,n,m1,n1)+visit(map,x,y+1,m,n,m1,n1);
+		}
+		return 0;
+	}
+}
